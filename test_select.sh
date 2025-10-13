cd622000e37e6311329e4daf0122d2b60a149c2d
Raphael Amorim <rapha850@gmail.com>
2025-10-14 01:54:22 +0200

    again removing stuff

--- test_select.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Test script for select functionality
-echo "Testing select statement parsing..."
-
-# Test 1: Basic select parsing
-echo "select choice in apple banana cherry; do echo \$choice; done" | ./target/debug/flash -c "$(cat)"
-
-echo "Select parsing test completed."
